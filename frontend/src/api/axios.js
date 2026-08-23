2e8f6e2013513a31752ef887a694f7d1a0fdd8f4
Sonia Noor <soniadev765@gmail.com>
2026-08-23 19:31:02 +0500

    fix: connect with live backend api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,29 +1,9 @@
+// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+
+  baseURL: "https://onrender.com", 
 });
-
-// ===============================
-// Automatically Send JWT Token
-// ===============================
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default api;
